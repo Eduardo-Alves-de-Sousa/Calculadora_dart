20feefa222748272922ae337a128fda3d4e335d0
Eduardo-Alves-de-Sousa <eduardoalvessousa031@gmail.com>
2024-04-16 13:43:28 -0300

    Corrigido: mudanças com relação a limpar os valores e pegue os valores da igualdade.

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,45 +1,63 @@
 class Memory {
-  static const operations = const ['%', '/', 'x', '-', '+', '='];
+  static const operations = ['%', '/', 'x', '-', '+', '='];
 
   final _buffer = [0.0, 0.0];
   int _bufferIndex = 0;
-  late String _operation;
+  String? _operation;
   String _value = '0';
   bool _wipeValue = false;
+  String? _lastCommand;
 
   void applyCommand(String command) {
-    if(command == 'AC'){
+    if (_isReplacingOperation(command)) {
+      _operation = command;
+      return;
+    }
+
+    if (command == 'AC') {
       _allClear();
-    }else if(operations.contains(command)){
+    } else if (operations.contains(command)) {
       _setOperation(command);
-    }else{
+    } else {
       _addDigit(command);
     }
+
+    _lastCommand = command;
   }
 
-  _setOperation(String newOperation){
-    if(_bufferIndex == 0){
-      _operation = newOperation;
-      _bufferIndex = 1;
-    }else{
+  _isReplacingOperation(String command) {
+    return operations.contains(_lastCommand) &&
+        operations.contains(command) &&
+        _lastCommand != '=' &&
+        command != '=';
+  }
+
+  _setOperation(String newOperation) {
+    bool isEqualSign = newOperation == '=';
+    if (_bufferIndex == 0) {
+      if (!isEqualSign) {
+        _operation = newOperation;
+        _bufferIndex = 1;
+        _wipeValue = true;
+      }
+    } else {
       _buffer[0] = _calculate();
       _buffer[1] = 0.0;
       _value = _buffer[0].toString();
       _value = _value.endsWith('.0') ? _value.split('.')[0] : _value;
 
-      bool isEqualSing = newOperation == '=';
-      _operation = (isEqualSing ? null : newOperation)!;
-      _bufferIndex = isEqualSing ? 0 : 1;
+      _operation = isEqualSign ? null : newOperation;
+      _bufferIndex = isEqualSign ? 0 : 1;
     }
 
-    _wipeValue = true;
+    _wipeValue = true; // !isEqualSign;
   }
 
-  _addDigit(String digit){
+  _addDigit(String digit) {
     final isDot = digit == '.';
     final wipeValue = (_value == '0' && !isDot) || _wipeValue;
 
-    if(isDot && _value.contains('.') && !wipeValue){
+    if (isDot && _value.contains('.') && !wipeValue) {
       return;
     }
 
@@ -51,22 +69,28 @@ class Memory {
     _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
   }
 
-  _allClear(){
+  _allClear() {
     _value = '0';
     _buffer.setAll(0, [0.0, 0.0]);
-    _operation = '';
+    _bufferIndex = 0;
+    _operation = null;
     _wipeValue = false;
-
   }
 
-  _calculate(){
-    switch(_operation){
-      case '%' : return _buffer[0] % _buffer[1];
-      case '/' : return _buffer[0] / _buffer[1];
-      case 'x' : return _buffer[0] * _buffer[1];
-      case '-' : return _buffer[0] - _buffer[1];
-      case '+' : return _buffer[0] + _buffer[1];
-      default: return _buffer[0];
+  _calculate() {
+    switch (_operation) {
+      case '%':
+        return _buffer[0] % _buffer[1];
+      case '/':
+        return _buffer[0] / _buffer[1];
+      case 'x':
+        return _buffer[0] * _buffer[1];
+      case '-':
+        return _buffer[0] - _buffer[1];
+      case '+':
+        return _buffer[0] + _buffer[1];
+      default:
+        return _buffer[0];
     }
   }
 
